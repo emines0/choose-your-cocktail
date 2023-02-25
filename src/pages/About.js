227ced5c03cefc8ee3970fd221031ded819a50af
emines0 <emin.sefa1@gmail.com>
2023-02-25 16:40:43 +0100

    about, error page implementation

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde facere
+        aut voluptate voluptatibus repudiandae odio molestias pariatur,
+        veritatis consequuntur laboriosam, suscipit esse dolore culpa in rem
+        nulla et? Dolorem, ab excepturi. Quasi quis voluptas repellat! Qui
+        repellendus, rem, asperiores adipisci excepturi totam aspernatur fugit,
+        incidunt aliquam commodi nam eveniet laudantium.
+      </p>
+    </section>
   )
 }
 
