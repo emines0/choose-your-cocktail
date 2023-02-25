2a6f5f2395212fa2a322d72dbfaa2913a7319e59
emines0 <emin.sefa1@gmail.com>
2023-02-25 16:53:32 +0100

    show/hide loading component, show/hide no matched criteria message

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -4,6 +4,25 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext()
+
+  if (loading) {
+    return <Loading />
+  }
+  /*
+   * if loading true show loading component
+   */
+  if (cocktails.length < 1) {
+    return (
+      <h2 className='section-title'>
+        no coctails matched your search criteria
+      </h2>
+    )
+  }
+  /*
+   * if the length of the coctails array is < 1 then show the message
+   */
+
   return (
     <div>
       <h2>cocktail list component</h2>
